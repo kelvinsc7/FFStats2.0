f26e1a4d341c57ce93d033fdcead5296b2237f22
kelvinsc7 <kelvincarvalho70@gmail.com>
2024-12-02 23:57:00 -0300

    Novas Mudanças

--- Front/FFStats-App/src/app/Components/partidas/partidas-detalhes/estatisticas-visualizar/estatisticas-visualizar.component.ts
+++ Front/FFStats-App/src/app/Components/partidas/partidas-detalhes/estatisticas-visualizar/estatisticas-visualizar.component.ts
@@ -15,6 +15,11 @@ export class EstatisticasVisualizarComponent implements OnInit {
   @Input() modo!: string;
   partidaId = +this.activatedRouter.snapshot.paramMap.get('id');
   stats:Estatistica[];
+  totalDano:number = 0
+  totalDerrubados:number = 0
+  totalCura:number = 0
+  totalLevantados:number = 0
+  totalRessucitou:number = 0
   
   constructor(    
     private estatisticaService: EstatisticaService,
@@ -26,6 +31,7 @@ export class EstatisticasVisualizarComponent implements OnInit {
 
   ngOnInit() {
     this.carregaDados();
+    
   }
   public async carregaDados(): Promise<void> {
     this.partidaId = +this.activatedRouter.snapshot.paramMap.get('id');
@@ -35,6 +41,8 @@ export class EstatisticasVisualizarComponent implements OnInit {
       try {
         // Aguarda a partida ser carregada
         this.stats = await this.estatisticaService.getEstatisticasByPartidaId(this.partidaId).toPromise();
+        await this.carregaTotais();
+        await this.carregaPercents()
       } catch (error) {
         console.error(error);
         this.toaster.error('Erro ao carregar dados', 'Erro!');
@@ -42,6 +50,28 @@ export class EstatisticasVisualizarComponent implements OnInit {
         this.spiner.hide();
       }
     }
+  }
+  public carregaTotais():void{
+    this.stats.forEach(e=> {
+      this.totalDano += e.dano;
+      this.totalDerrubados +=e.derrubado
+      this.totalCura +=e.cura
+      this.totalLevantados +=e.levantados
+      this.totalRessucitou +=e.ressucitou
+    });
+  }
+  public carregaPercents():void{
+    this.stats.forEach(e=>{
+
+      if (!e.porcentagem) {
+        e.porcentagem = {};
+      }
+      e.porcentagem.percentDano = (e.dano *100)/this.totalDano;
+      e.porcentagem.percentDerrubado = (e.derrubado *100)/this.totalDerrubados;
+      e.porcentagem.percentCura = (e.cura *100)/this.totalCura;
+      e.porcentagem.percentLevantados = (e.levantados *100)/this.totalLevantados;
+      e.porcentagem.percentRessucitou = (e.ressucitou *100)/this.totalRessucitou; 
+    })
   }
 
 }
