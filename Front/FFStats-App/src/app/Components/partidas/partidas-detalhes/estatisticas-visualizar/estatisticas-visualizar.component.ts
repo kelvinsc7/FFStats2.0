d10fc7f4b121f323ff30edf95574cd652cdd829f
kelvinsc7 <kelvincarvalho70@gmail.com>
2024-12-05 23:28:14 -0300

    Tela Finalizada

--- Front/FFStats-App/src/app/Components/partidas/partidas-detalhes/estatisticas-visualizar/estatisticas-visualizar.component.ts
+++ Front/FFStats-App/src/app/Components/partidas/partidas-detalhes/estatisticas-visualizar/estatisticas-visualizar.component.ts
@@ -23,6 +23,7 @@ export class EstatisticasVisualizarComponent implements OnInit {
   totalRessucitou:number = 0
   currentView: string = 'visualizar';
   isLoading = false;
+  idMvp:number = 0;
   
   constructor(    
     private estatisticaService: EstatisticaService,
@@ -52,6 +53,7 @@ export class EstatisticasVisualizarComponent implements OnInit {
         });
         await this.carregaTotais();
         await this.carregaPercents()
+        this.idMvp =  this.calcularMVP(this.stats)
       } catch (error) {
         console.error(error);
         this.toaster.error('Erro ao carregar dados', 'Erro!');
@@ -90,6 +92,62 @@ export class EstatisticasVisualizarComponent implements OnInit {
       this.partidaService.setViewMode('editar');
 
     }
+  }
+  private calcularMVP(estatisticas: Estatistica[]): number | null {
+    if (!estatisticas || estatisticas.length === 0) {
+      return null; // Sem estatísticas, não há MVP
+    }
+  
+    let maiorPontuacao = -Infinity;
+    let MVP = null;
+  
+    estatisticas.forEach(estatistica => {
+      const pontuacao = this.calcularPontuacao(estatistica);
+  
+      if (pontuacao > maiorPontuacao) {
+        maiorPontuacao = pontuacao;
+        MVP = estatistica.jogador.id; 
+      }
+    });
+  
+    return MVP;
+  }
+  private calcularPontuacao(estatistica: Estatistica): number {
+    const tempoString = this.converteToTime(estatistica.tempo) || "0:00"; // Pega o valor ou um padrão "0:00"
+    const minutos = this.extrairMinutos(tempoString); // Extrai os minutos como um número
+
+    const kills = estatistica.kill || 0;
+    const mortes = estatistica.morte || 0;
+    const assistencias = estatistica.assistencia || 0;
+    const dano = estatistica.dano || 0;
+    const derrubados = estatistica.derrubado || 0;
+    const cura = estatistica.cura || 0;
+    const levantou = estatistica.levantados || 0;
+    const ressuscitou = estatistica.ressucitou || 0;
+  
+    return (
+      (kills * 4) +
+      (mortes * -2) +
+      (assistencias * 2) +
+      (dano / 100) +
+      (derrubados * 1) +
+      (cura / 50) +
+      (levantou * 3) +
+      (ressuscitou * 5) +
+      (minutos * 1)
+    );
+  }
+  private extrairMinutos(tempo: string): number {
+    const [minutos] = tempo.split(':').map(Number); // Divide a string e converte a primeira parte (minutos)
+    return minutos || 0; // Retorna 0 se os minutos não forem válidos
+  }
+  converteToTime(seconds: string): string {
+    const minutes = Math.floor(parseInt(seconds) / 60);
+    const remainingSeconds = parseInt(seconds) % 60;
+    return `${this.padZero(minutes)}:${this.padZero(remainingSeconds)}`;
+  }
+  private padZero(num: number): string {
+    return num < 10 ? `0${num}` : `${num}`;
   }
 
 }
